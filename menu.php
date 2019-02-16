7fe99033d2fc319c67f4c6bc08f39621005ec2e8
Your Name <your_email@whatever.com>
2019-02-16 10:35:08 +0500

    Pervoe izmenenie

---
+++ menu.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+<a href="https://www.example.com/index.php">Главная</a> - 
+<a href="https://www.example.com/about.php">О нас</a> - 
+<a href="https://www.example.com/links.php">Ссылки</a> - 
+<a href="https://www.example.com/contact.php">Контакты</a> <br />
